b1208fc2d35e8580ac02dbb98ecfc8e1384b8c23
1gitpoint <162420664+1gitpoint@users.noreply.github.com>
2024-03-07 11:13:04 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,1 @@
-print("============================INSTALL STABLE")
-try:
-    import traceback,platform
-    traceback.print_stack()
-    print(platform.system())
-except Exception as e:
-    print(e)
-  
-print("============================END INSTALL")
-raise ValueError()
+
